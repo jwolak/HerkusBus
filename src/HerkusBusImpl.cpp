5cf6fd9150d8915b66984e6c79a04538b3297b8c
Janusz <januszvdm@gmail.com>
2025-08-30 18:08:49 +0200

    Refactor debug logging in HerkusBusImpl for improved clarity and consistency

--- src/HerkusBusImpl.cpp
+++ src/HerkusBusImpl.cpp
@@ -65,40 +65,40 @@ const std::string kIpcConditionVariableName = "HerkusIpcConditionVariable";
 HerkusBusImpl::HerkusBusImpl() : bus_event_loop_thread_{}, stop_listener_event_loop_{false}, subscribers_callbacks_{}, subscribers_mutex_{} {
   spdlog::set_level(spdlog::level::debug);
   boost::interprocess::shared_memory_object::remove(kSharedMemoryName.c_str());
-  spdlog::debug("Shared memory segment removed", __FILENAME__, __LINE__);
+  spdlog::debug("Shared memory segment removed [{0}:{1}]", __FILENAME__, __LINE__);
   shared_memory_segment_ = managed_shared_memory{open_or_create, kSharedMemoryName.c_str(), kSharedMemorySize};
   message_queue_ = shared_memory_segment_.find_or_construct<shared_mem_message_deque>(kMessageQueueName.c_str())(
       shared_mem_allocator(shared_memory_segment_.get_segment_manager()));
   ipc_mtx_ = shared_memory_segment_.find_or_construct<interprocess_mutex>(kIpcMutexName.c_str())();
   ipc_condition_variable_ = shared_memory_segment_.find_or_construct<interprocess_condition>(kIpcConditionVariableName.c_str())();
 
-  spdlog::debug("Create bus even loop thread...", __FILENAME__, __LINE__);
+  spdlog::debug("Create bus event loop thread... [{0}:{1}]", __FILENAME__, __LINE__);
   bus_event_loop_thread_ = std::thread([this]() {
     while (!stop_listener_event_loop_) {
       scoped_lock<interprocess_mutex> lock(*ipc_mtx_);
-      spdlog::debug("Event loop checks if message queue is empty...", __FILENAME__, __LINE__);
+      spdlog::debug("Event loop checks if message queue is empty... [{0}:{1}]", __FILENAME__, __LINE__);
       if (message_queue_->empty()) {
-        spdlog::debug("No message in queue", __FILENAME__, __LINE__);
-        spdlog::debug("Waiting...", __FILENAME__, __LINE__);
+        spdlog::debug("No message in queue [{0}:{1}]", __FILENAME__, __LINE__);
+        spdlog::debug("Waiting... [{0}:{1}]", __FILENAME__, __LINE__);
         ipc_condition_variable_->wait(lock);
         if (stop_listener_event_loop_) {
-          spdlog::debug("Event loop stopped", __FILENAME__, __LINE__);
+          spdlog::debug("Event loop stopped [{0}:{1}]", __FILENAME__, __LINE__);
           break;
         }
       }
-      spdlog::debug("Message queue is not empty...", __FILENAME__, __LINE__);
+      spdlog::debug("Message queue is not empty... [{0}:{1}]", __FILENAME__, __LINE__);
       while (!message_queue_->empty()) {
-        spdlog::debug("New message in queue", __FILENAME__, __LINE__);
+        spdlog::debug("New message in queue [{0}:{1}]", __FILENAME__, __LINE__);
         Message msg = message_queue_->front();
-        spdlog::debug("Get message from queue", __FILENAME__, __LINE__);
+        spdlog::debug("Get message from queue [{0}:{1}]", __FILENAME__, __LINE__);
         message_queue_->pop_front();
-        spdlog::debug("Message removed from queue as already taken", __FILENAME__, __LINE__);
+        spdlog::debug("Message removed from queue as already taken [{0}:{1}]", __FILENAME__, __LINE__);
         lock.unlock();
 
-        spdlog::debug("Parsing message...", __FILENAME__, __LINE__);
+        spdlog::debug("Parsing message... [{0}:{1}]", __FILENAME__, __LINE__);
         auto parsed_msg = json::parse(msg.payload);
 
-        spdlog::debug("Call callbacks for all subscribers subscribed on topic: [{0}]", msg.topic, __FILENAME__, __LINE__);
+        spdlog::debug("Call callbacks for all subscribers subscribed on topic: [{0}] [{1}:{2}]", msg.topic, __FILENAME__, __LINE__);
         auto it = [&] {
           std::lock_guard<std::mutex> lock(subscribers_mutex_);
           return subscribers_callbacks_.find(msg.topic);
@@ -117,7 +117,7 @@ HerkusBusImpl::HerkusBusImpl() : bus_event_loop_thread_{}, stop_listener_event_l
 HerkusBusImpl::~HerkusBusImpl() {
   stop_listener_event_loop_ = true;
   scoped_lock<interprocess_mutex> lock(*ipc_mtx_);
-  spdlog::debug("Notify bus event loop to be stopped", __FILENAME__, __LINE__);
+  spdlog::debug("Notify bus event loop to be stopped [{0}:{1}]", __FILENAME__, __LINE__);
   ipc_condition_variable_->notify_all();
   if (bus_event_loop_thread_.joinable()) {
     bus_event_loop_thread_.join();
@@ -125,19 +125,19 @@ HerkusBusImpl::~HerkusBusImpl() {
 }
 
 void HerkusBusImpl::Publish(const std::string& topic, const json& message_payload) {
-  spdlog::debug("Publish message on topic: {0}", topic, __FILENAME__, __LINE__);
+  spdlog::debug("Publish message on topic: {0} [{1}:{2}]", topic, __FILENAME__, __LINE__);
   std::string payload = message_payload.dump();
   scoped_lock<interprocess_mutex> lock(*ipc_mtx_);
-  spdlog::debug("Add message to message queue", __FILENAME__, __LINE__);
+  spdlog::debug("Add message to message queue [{0}:{1}]", __FILENAME__, __LINE__);
   message_queue_->emplace_back(Message{topic, payload});
-  spdlog::debug("Notify bus event loop about new published message...", __FILENAME__, __LINE__);
+  spdlog::debug("Notify bus event loop about new published message... [{0}:{1}]", __FILENAME__, __LINE__);
   ipc_condition_variable_->notify_one();
 }
 
 void HerkusBusImpl::Subscribe(const std::string& topic, subscriber_callback sub_callback) {
-  spdlog::debug("Subscribe on topic: {0}", topic, __FILENAME__, __LINE__);
+  spdlog::debug("Subscribe on topic: {0} [{1}:{2}]", topic, __FILENAME__, __LINE__);
   std::lock_guard<std::mutex> lock(subscribers_mutex_);
-  spdlog::debug("Add new subscriber callback to callbacks list", topic, __FILENAME__, __LINE__);
+  spdlog::debug("Add new subscriber callback to callbacks list [{0}:{1}]", topic, __FILENAME__, __LINE__);
   subscribers_callbacks_[topic].push_back(std::move(sub_callback));
 }
 }  // namespace Herkus
