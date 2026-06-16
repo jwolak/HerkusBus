fa5f83662a422c0c9af49f82d60d5ee996666d5a
Janusz <januszvdm@gmail.com>
2026-06-16 20:04:16 +0200

    Refactor HerkusBusMockedExample to improve class structure and method calls

--- examples/HerkusBusMockedExample.cpp
+++ examples/HerkusBusMockedExample.cpp
@@ -14,22 +14,33 @@ class HerkusBusMock : public Herkus::IHerkusBus {
   MOCK_METHOD(void, Subscribe, (const std::string& topic, Herkus::subscriber_callback sub_callback), (override));
 };
 
-class ClassWithMockedHerkusBus {
+class ClassWithHerkusBus {
  public:
-  ClassWithMockedHerkusBus(std::unique_ptr<Herkus::IHerkusBus> herkus_bus) : herkus_bus_(std::move(herkus_bus)) {}
+  ClassWithHerkusBus() : ClassWithHerkusBus(Herkus::HerkusBus::getInstance()) {}
+
+ protected:
+  explicit ClassWithHerkusBus(Herkus::IHerkusBus& herkus_bus) : herkus_bus_(herkus_bus) {}
 
   void MethodThatCallsHerkusBusPublish(const std::string& topic, const Herkus::json& message_payload) {
     spdlog::info("Publishing message to topic: {}", topic);
-    herkus_bus_->Publish(topic, message_payload);
+    herkus_bus_.Publish(topic, message_payload);
   }
 
   void MethodThatCallsHerkusBusSubscribe(const std::string& topic, Herkus::subscriber_callback sub_callback) {
     spdlog::info("Subscribing to topic: {}", topic);
-    herkus_bus_->Subscribe(topic, std::move(sub_callback));
+    herkus_bus_.Subscribe(topic, std::move(sub_callback));
   }
 
  private:
-  std::unique_ptr<Herkus::IHerkusBus> herkus_bus_;
+  Herkus::IHerkusBus& herkus_bus_;
+};
+
+class ClassWithMockedBus : public ClassWithHerkusBus {
+ public:
+  ClassWithMockedBus(Herkus::IHerkusBus& herkus_bus) : ClassWithHerkusBus(herkus_bus) {}
+
+  using ClassWithHerkusBus::MethodThatCallsHerkusBusPublish;
+  using ClassWithHerkusBus::MethodThatCallsHerkusBusSubscribe;
 };
 
 }  // namespace HerkusBuxMockedExample
@@ -38,8 +49,7 @@ using namespace testing;
 
 int main() {
   StrictMock<HerkusBuxMockedExample::HerkusBusMock>* mock_bus_mock = new StrictMock<HerkusBuxMockedExample::HerkusBusMock>();
-  HerkusBuxMockedExample::ClassWithMockedHerkusBus class_with_mocked_bus =
-      HerkusBuxMockedExample::ClassWithMockedHerkusBus(std::unique_ptr<Herkus::IHerkusBus>(mock_bus_mock));
+  HerkusBuxMockedExample::ClassWithMockedBus class_with_mocked_bus(*mock_bus_mock);
 
   EXPECT_CALL(*mock_bus_mock, Publish("topic1", Herkus::json::object({{"key", "value"}})));
   EXPECT_CALL(*mock_bus_mock, Subscribe("topic2", _));
@@ -48,6 +58,8 @@ int main() {
   class_with_mocked_bus.MethodThatCallsHerkusBusSubscribe("topic2", [](const std::string& topic, const Herkus::json& msg) {
     std::cout << "Received message on topic: " << topic << " with payload: " << msg.dump() << std::endl;
   });
+
+  delete mock_bus_mock;
 
   return 0;
 }
